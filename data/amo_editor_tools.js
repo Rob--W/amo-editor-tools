471c09bb49deec4d471d639989d6f3b1c7a47223
Rob Wu <rob@robwu.nl>
2016-12-15 02:02:38 +0100

    Initial version
    Ported from my GreaseMonkey script because GM_xmlHttpRequest does
    not support cross-origin cookies, nor getting the headers at
    readyState 2.
    
    This does still not work because of https://bugzil.la/1310082.
    So I will convert it to a SDK add-on...
    Hopefully I can revert to this patch in the future.

---
+++ data/amo_editor_tools.js
@@ -0,0 +1,155 @@
+// en-US, en-GB, etc.
+const path = /^\/(?:[^\/]+)\/editors\/(.*)/i.exec(location.pathname)[1];
+// Cache up to CACHE_LENGTH items in localStorage[CACHE_STORAGE_KEY]
+const CACHE_LENGTH = 500;
+const CACHE_STORAGE_KEY = 'robsAmoEditorToolsCache';
+console.log(path)
+
+if (path.startsWith('queue/')) {
+  expandQueueTable();
+}
+
+function getSlug(url) {
+  let m = /\/editors\/review\/([^\/?#]+)/.exec(url);
+  return m && m[1];
+}
+
+// cached is sorted by access time - last accessed at front.
+function getSlugCache(slug) {
+  let cached = localStorage[CACHE_STORAGE_KEY] || '[]';
+  cached = JSON.parse(cached);
+  let index = cached.findIndex(o => o.s === slug);
+  return [cached, index];
+}
+
+function saveForSlug(slug, version, data) {
+  let [cached, index] = getSlugCache(slug);
+  let item = {s: slug, v: version, d: data};
+  if (index === -1) {
+    if (cached.length > CACHE_LENGTH) {
+      cached.length = CACHE_LENGTH;
+    }
+  } else {
+    cached.splice(index, 1);
+  }
+  cached.unshift(item);
+  localStorage[CACHE_STORAGE_KEY] = JSON.stringify(cached);
+}
+
+function readForSlug(slug, version) {
+  let [cached, index] = getSlugCache(slug);
+  if (index >= 0) {
+    let item = cached[index];
+    if (item.v === version) {
+      return item.d;
+    }
+  }
+}
+
+function formatByteSize(fileSize) {
+  // Assume parameter fileSize to be a number
+  fileSize = (fileSize+'').replace(/\d(?=(\d{3})+(?!\d))/g, '$&,');
+  return fileSize;
+}
+
+var runonce;
+function fetchAddonInfo(slug, onResult) {
+  if (runonce) return;
+  runonce = true;
+  // For logic of URL, see https://github.com/Rob--W/crxviewer/blob/fb91e4a37de2a4ee36c925076473e5044c982915/src/cws_pattern.js#L36-L62
+  let platformIds = [
+    3, // Mac
+    2, // Linux
+    5, // Windows
+    7, // Android
+  ];
+  
+  doFetch();
+  function doFetch() {
+    let platformId = platformIds.shift();
+    if (platformId === undefined) {
+      onResult(-1, 'Not a zip file');
+      return;
+    }
+    let xpiUrl = `https://addons.mozilla.org/firefox/downloads/latest/${slug}/platform:${platformId}/${slug}.xpi`;
+    ZipInfo.getRemoteEntries(xpiUrl, function(entries) {
+      if (entries.length === 1) {
+        doFetch(); // Invalid zip.
+        return;
+      }
+      let jsSize = 0;       // JS, JSM
+      let jsLibSize = 0;    // Known library **names**
+      let markupSize = 0;   // HTML, XUL
+      let resourceSize = 0; // Images, fonts, CSS
+      let miscSize = 0;     // All other things.
+      let miscExtSizes = Object.create(null);
+      for (let {filename, uncompressedSize} of entries.filter(entry => !entry.directory)) {
+        let ext = filename.split('.').pop().toLowerCase();
+        if (['js', 'jsm'].includes(ext)) {
+          jsSize += uncompressedSize;
+          // Some of the files are listed at https://github.com/mozilla/amo-validator/blob/master/validator/testcases/hashes-allowed.txt
+          // bootstrap.js is ambiguous: Either bootstrapped add-ons or Twitter Bootstrap.
+          if (/jquery|angular|bootstrap|react|backbone|underscore|moment/i.test(filename) && filename !== 'bootstrap.js') {
+            jsLibSize += uncompressedSize;
+          }
+        } else if (['html', 'htm', 'xhtml', 'xul', 'dtd', 'xml'].includes(ext)) {
+          markupSize += uncompressedSize;
+        } else if ([
+          // Images (note: svg is also a font).
+          'png', 'jpeg', 'jpg', 'gif', 'bmp', 'ico', 'svg',
+          // Fonts
+          'woff', 'woff2', 'ttf', 'eot',
+          // CSS
+          'css',
+        ].includes(ext)) {
+          resourceSize += uncompressedSize;
+        } else {
+          miscSize += uncompressedSize;
+          miscExtSizes[ext] = (miscExtSizes[ext] + uncompressedSize) || uncompressedSize;
+        }
+      }
+      // Sort by extensions, biggest size first.
+      let sortedMiscExts = Object.keys(miscExtSizes).sort((extA, extB) => miscExtSizes[extB] - miscExtSizes[extA]);
+      let relevantSize = jsSize - jsLibSize;
+      let title = [
+        'JS (no libraries):\t' + formatByteSize(relevantSize),
+        'JS libraries only:\t' + formatByteSize(jsLibSize),
+        'Markup (HTML/XUL):\t' + formatByteSize(markupSize),
+        'Static (fonts/images/...):\t' + formatByteSize(resourceSize),
+        'Other file sizes:\t' + formatByteSize(resourceSize),
+        'Other extensions = ' + sortedMiscExts.join(', '),
+      ].join('\n');
+      onResult(relevantSize, title);
+    });
+  }
+}
+
+function expandQueueTable() {
+  let table = document.getElementById('addon-queue');
+  table.tHead.rows[0].insertCell(-1).textContent = 'Size';
+  let {rows} = table.tBodies[0];
+  [].forEach.call(rows, row => {
+    let reviewLink = row.querySelector('a[href*="/editors/review/"]');
+    let version = reviewLink.querySelector('em').textContent.trim();
+    let slug = getSlug(reviewLink.href);
+    if (!slug) {
+      throw new Error('Slug not found: ' + reviewLink.href);
+    }
+    let onResult = (relevantSize, title) => {
+      // Always save, so that the latest read value is revived and
+      // is not deleted when the LRU queue is full.
+      saveForSlug(slug, version, {relevantSize, title});
+      // Insert before last cell, which seems always empty (styling?).
+      let cell = row.insertCell(row.cells.length - 1);
+      cell.style.textAlign = 'right';
+      cell.style.width = '9ch';
+      cell.textContent = formatByteSize(relevantSize);
+    };
+    let cached = readForSlug(slug, version);
+    if (cached) {
+      onResult(cached.relevantSize, cached.title);
+    } else {
+      fetchAddonInfo(slug, onResult);
+    }
+  });
+}
---
+++ data/zipinfo.js
@@ -0,0 +1,176 @@
+/**
+ * Copyright (c) 2016 Rob Wu <rob@robwu.nl> (https://robwu.nl)
+ * Published under a MIT license.
+ * https://github.com/Rob--W/zipinfo.js
+ **/
+/* globals DataView, TextDecoder, Buffer, module */
+'use strict';
+var ZipInfo = typeof module === 'object' && module.exports || {};
+
+/**
+ * Reports the metadata of a zip file. This method itself is resilient against
+ * malformed zip files, but you should take any result with a pinch of salt
+ * since it is trivial to spoof the metadata (which often breaks unzipping).
+ *
+ * The file must match the format as specified by:
+ * https://en.wikipedia.org/wiki/Zip_(file_format)
+ * https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
+ *
+ * The following zip features are not supported:
+ * - ZIP64
+ * - Encrypted zip files
+ * - Data description headers (=all file sizes will be reported as 0).
+ *
+ * @param {Uint8Array} data - Valid zip data. This may start anywhere in the zip
+ *    file, as long as the end of the data is also the end of the zip file.
+ * @param {number} [dataStartOffset=0] - The position in the zip file where the
+ *    data starts. The list of files if a zip file is usually stored at the end,
+ *    so usually it suffices to first try to find the EOCD record that specifies
+ *    the start of the central directory (which lists all files), and then fetch
+ *    the data if needed.
+ * @returns {object} A list of objects describing each entry:
+ * - directory (boolean) - whether the entry is a directory.
+ * - filename (string) - name of entry.
+ * - uncompressedSize (number) - the size of the entry when uncompressed.
+ * The first entry is always an artificial '/' directory, and includes the
+ * following property:
+ * - centralDirectoryStart (number) - The start of the central directory as
+ *   claimed by the zip file. If `data` is only a part of the zip file, you
+ *   should check whether `centralDirectoryStart < dataStartOffset`, and if so
+ *   fetch more data starting from `dataStartOffset` and call this method
+ *   again. Otherwise the returned list of files may be incomplete.
+ */
+ZipInfo.getEntries = function(data, dataStartOffset) {
+  var view = new DataView(data.buffer, data.byteOffset, data.length);
+  var entriesLeft = 0;
+  var offset = 0;
+  var endoffset = data.length;
+  // Find EOCD (0xFFFF is the maximum size of an optional trailing comment).
+  for (var i = data.length - 22, ii = Math.max(0, i - 0xFFFF); i >= ii; --i) {
+    if (data[i] === 0x50 && data[i + 1] === 0x4b &&
+      data[i + 2] === 0x05 && data[i + 3] === 0x06) {
+        endoffset = i;
+        offset = view.getUint32(i + 16, true);
+        entriesLeft = view.getUint16(i + 8, true);
+        break;
+      }
+  }
+  var entries = [{
+    directory: true,
+    filename: '/',
+    uncompressedSize: 0,
+    centralDirectoryStart: offset,
+  }];
+  if (dataStartOffset) {
+    offset -= dataStartOffset;
+  }
+  if (offset >= data.length || offset <= 0) {
+    // EOCD not found or malformed. Try to recover if possible (the result is
+    // most likely going to be incomplete or bogus, but we can try...).
+    offset = -1;
+    entriesLeft = 0xFFFF;
+    while (++offset < data.length && data[offset] !== 0x50 &&
+      data[offset + 1] !== 0x4b && data[offset + 2] !== 0x01 &&
+        data[offset + 3] !== 0x02);
+  }
+  endoffset -= 46;  // 46 = minimum size of an entry in the central directory.
+  while (--entriesLeft >= 0 && offset < endoffset) {
+    if (view.getUint32(offset) != 0x504b0102) {
+      break;
+    }
+    var bitFlag = view.getUint16(offset + 8, true);
+    var uncompressedSize = view.getUint32(offset + 24, true);
+    var fileNameLength = view.getUint16(offset + 28, true);
+    var extraFieldLength = view.getUint16(offset + 30, true);
+    var fileCommentLength = view.getUint16(offset + 32, true);
+    var filename = data.subarray(offset + 46, offset + 46 + fileNameLength);
+    var utfLabel = (bitFlag & 0x800) ? 'utf-8' : 'ascii';
+    filename = ZipInfo._decodeFilename(filename, utfLabel);
+
+    entries.push({
+      directory: filename.endsWith('/'),
+      filename: filename,
+      uncompressedSize: uncompressedSize,
+    });
+    offset += 46 + fileNameLength + extraFieldLength + fileCommentLength;
+  }
+  return entries;
+};
+
+/**
+ * @param {Uint8Array} filename
+ * @param {string} utfLabel
+ */
+ZipInfo._decodeFilename = function(filename, utfLabel) {
+  if (typeof TextDecoder == 'function') {
+    return new TextDecoder(utfLabel).decode(filename);
+  }
+  return new Buffer(filename).toString(utfLabel);  // Node.js
+};
+
+/**
+ * Fetches the list of files in a zip file using the HTTP protocol (http/https).
+ *
+ * @param {function} sendHttpRequest - The method to fetch the initial data.
+ *  This callback is passed an object with the following properties:
+ *  - rangeHeader - An optional string. If not falsey, the "Range" request
+ *    header must be set on the request with this value.
+ *  - onHeadersReceived - This method should be called when the headers become
+ *    available. Calling this is recommended but not required. The callback
+ *    should be passed a function that returns a header for a given header name,
+ *    or a falsey value if the header is unavailable.
+ *  - onCompleted - This method must be called when the request finishes, UNLESS
+ *    the request is explicitly aborted. The callback should be called with a
+ *    Uint8Array of the response (which may be empty if an error has occurred).
+ *  The method must return an object with the "abort" method, which cancels the
+ *  initial request.
+ * @param {function} onGotEntries - Called when all request finish. The method
+ *  is called with a single argument, the return value of ZipInfo.getEntries.
+ */
+ZipInfo.runGetEntriesOverHttp = function(sendHttpRequest, onGotEntries) {
+  function getRange(start, length) {
+    // We are expecting a response at the end of a zip file. Do not set the
+    // range header if the start is 0 in case the server has a buggy range
+    // request implementation.
+    if (start) {
+      return 'bytes=' + start + '-' + (length - 1) + '/' + length;
+    }
+  }
+
+  var x = sendHttpRequest({
+    onHeadersReceived: function(getResponseHeader) {
+      var length = parseInt(getResponseHeader('Content-Length'), 10) || 0;
+      // 100k is an arbitrary threshold above the maximum EOCD record size.
+      if (length < 1e5 || getResponseHeader('Accept-Ranges') !== 'bytes') {
+        return;
+      }
+      // Switch to range requests.
+      x.abort();
+      // The EOCD record size is at most 0xFFFF + 22. -1 for range request.
+      var start = length - 0xFFFF - 23;
+      sendHttpRequest({
+        rangeHeader: getRange(start, length),
+        onCompleted: function(response) {
+          if (start && response.byteLength === length) {
+            start = 0;  // Server does not seem to support range requests.
+          }
+          var entries = ZipInfo.getEntries(response, start);
+          if (entries[0].centralDirectoryStart >= start) {
+            onGotEntries(entries);
+          } else {
+            start = entries[0].centralDirectoryStart;
+            sendHttpRequest({
+              rangeHeader: getRange(start, length),
+              onCompleted: function(response) {
+                onGotEntries(ZipInfo.getEntries(response, start));
+              },
+            });
+          }
+        },
+      });
+    },
+    onCompleted: function(response) {
+      onGotEntries(ZipInfo.getEntries(response));
+    },
+  });
+};
